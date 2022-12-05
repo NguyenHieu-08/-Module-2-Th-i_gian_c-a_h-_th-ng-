859a7df64c7b9832dae23c0f9359973b834dc229
Hiếu Nguyễn <hieunghuu08@gmail.com>
2022-12-05 14:23:17 +0700

    Upload thời gian hệ thống

---
+++ ToDay.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ToDay {
+    public static void main(String[] args) {
+        Date day = new Date();
+        System.out.println("Thời gian hiện tại" + day);
+    }
+}
